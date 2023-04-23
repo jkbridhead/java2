bb6b1ea2d1719123bcd4627292e7dc29db21bcc2
bloody-skyle <sihun0828@gmail.com>
2023-04-23 11:52:43 +0900

    자바

--- java-master/src/lecture_20230423/lol6.java
+++ java-master/src/lecture_20230423/lol6.java
@@ -1,16 +1,16 @@
+
 package lecture_20230423;
 
 import java.util.Scanner;
 
-public class lol6
-{
+public class lol6 {
     public static void main(String[] args) {
         System.out.println("대결을 시작힙니까? 1:시작, 2:탈주");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        switch (a){
+        switch (a) {
             case 2:
-             break;
+                break;
 
             case 1:
                 try {
@@ -19,28 +19,29 @@ public class lol6
                     // 예외 처리
                 }
                 System.out.println("미니언이 생성되었습니다.");
-                int rand = (int)(Math.random()*100);
-                int zedhp =700;
+                int rand = (int) (Math.random() * 100);
+                int zedhp = 700;
                 int Zedak = 150;
                 int Corkiak = 120;
                 int Corkihp = 1000;
 
-                while (Corkihp>0|| zedhp>0){
-                   int Corki = Corkiak-rand;
-                   int zed = Zedak - rand;
+                while (Corkihp > 0 && zedhp > 0) {
+                    int Corki = Corkiak - rand;
+                    int zed = Zedak - rand;
                     zedhp = zedhp - Corki;
-                    System.out.println("코르키 -> 제드 "+Corki+"의 피해 (코르키 체력:"+Corkihp+", 제드체력: "+zedhp+")");
+                    System.out.println("코르키 -> 제드 " + Corki + "의 피해 (코르키 체력:" + Corkihp + ", 제드체력: " + zedhp + ")");
                     Corkihp = Corkihp - zed;
-                    System.out.println("제드 -> 코르키 "+zed+"의 피해 (코르키 체력:"+Corkihp+", 제드체력: "+zedhp+")");
-                    rand = (int)(Math.random()*100);
-                    try {
-                        Thread.sleep(2000); // 2초 대기
-                    } catch (InterruptedException e) {
-                        // 예외 처리
+                    System.out.println("제드 -> 코르키 " + zed + "의 피해 (코르키 체력:" + Corkihp + ", 제드체력: " + zedhp + ")");
+                    rand = (int) (Math.random() * 100);
+                    if (Corkihp <= 0) {
+                        Corkihp = 0;
+                        System.out.println("제드 라인전 승리");
+                    }
+                    if (zedhp <= 0) {
+                        zedhp = 0;
+                        System.out.println("코르키 라인전 승리");
                     }
                 }
         }
     }
-
-
 }
